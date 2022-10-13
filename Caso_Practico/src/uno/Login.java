491852da74d5221a688e816830b0acdc83d7491d
Jesus <jrgaleasantiago98@gmail.com>
2022-10-13 13:08:31 +0200

    1

--- Caso_Practico/src/uno/Login.java
+++ Caso_Practico/src/uno/Login.java
@@ -15,12 +15,15 @@ import javax.swing.JPasswordField;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import java.awt.SystemColor;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Login extends JFrame {
 
 	private JPanel contentPane;
-	private JPasswordField passwordField;
-	private JTextField txtIntroduzcaSuUsuario;
+	private JPasswordField txtPass;
+	private JTextField txtUser;
 
 	/**
 	 * Launch the application.
@@ -42,6 +45,7 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		setUndecorated(true);
 		setBackground(Color.DARK_GRAY);
 		setLocationByPlatform(true);
 		setIconImage(Toolkit.getDefaultToolkit().getImage(Login.class.getResource("/image/icon.jpg")));
@@ -56,42 +60,68 @@ public class Login extends JFrame {
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.WHITE);
-		panel.setBounds(0, 0, 540, 298);
+		panel.setBounds(0, 0, 556, 337);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		passwordField = new JPasswordField();
-		passwordField.setBorder(null);
-		passwordField.setBounds(40, 166, 189, 20);
-		panel.add(passwordField);
-		
-		JLabel JL2 = new JLabel("Contraseña:");
-		JL2.setFont(new Font("Roboto", Font.PLAIN, 15));
-		JL2.setBounds(40, 129, 161, 30);
-		panel.add(JL2);
-		
-		JLabel JL1 = new JLabel("Usuario:");
-		JL1.setFont(new Font("Roboto", Font.PLAIN, 15));
-		JL1.setBounds(40, 51, 94, 25);
-		panel.add(JL1);
-		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(SystemColor.controlHighlight);
 		panel_1.setBorder(null);
-		panel_1.setBounds(0, 0, 325, 208);
+		panel_1.setBounds(0, 0, 325, 248);
 		panel.add(panel_1);
 		panel_1.setLayout(null);
 		
-		txtIntroduzcaSuUsuario = new JTextField();
-		txtIntroduzcaSuUsuario.setName("");
-		txtIntroduzcaSuUsuario.setText("Introduzca su usuario\r\n");
-		txtIntroduzcaSuUsuario.setCaretColor(Color.LIGHT_GRAY);
-		txtIntroduzcaSuUsuario.setBorder(null);
-		txtIntroduzcaSuUsuario.setFont(new Font("Roboto", Font.PLAIN, 11));
-		txtIntroduzcaSuUsuario.setForeground(Color.LIGHT_GRAY);
-		txtIntroduzcaSuUsuario.setToolTipText("Introduzca su usuario\r\n");
-		txtIntroduzcaSuUsuario.setBounds(37, 85, 190, 20);
-		panel_1.add(txtIntroduzcaSuUsuario);
-		txtIntroduzcaSuUsuario.setColumns(10);
+		txtUser = new JTextField();
+		txtUser.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+			txtUser.setText("");
+			txtUser.setForeground(Color.BLACK);
+			txtPass.setText("********");
+			txtPass.setForeground(Color.LIGHT_GRAY);
+			}
+		});
+		txtUser.setText("Introduzca su usuario");
+		txtUser.setName("");
+		txtUser.setCaretColor(Color.LIGHT_GRAY);
+		txtUser.setBorder(null);
+		txtUser.setFont(new Font("Roboto", Font.PLAIN, 11));
+		txtUser.setForeground(Color.LIGHT_GRAY);
+		txtUser.setToolTipText("Introduzca su usuario\r\n");
+		txtUser.setBounds(37, 95, 190, 20);
+		panel_1.add(txtUser);
+		txtUser.setColumns(10);
+		
+		JLabel JL1 = new JLabel("Usuario:");
+		JL1.setBounds(37, 59, 94, 25);
+		panel_1.add(JL1);
+		JL1.setFont(new Font("Roboto", Font.PLAIN, 15));
+		
+		txtPass = new JPasswordField();
+		txtPass.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				txtPass.setText("");
+				txtPass.setForeground(Color.BLACK);
+				txtUser.setText("Introduzca su usuario");
+				txtUser.setForeground(Color.LIGHT_GRAY);
+			}
+		});
+		txtPass.setForeground(Color.LIGHT_GRAY);
+		txtPass.setText("********");
+		txtPass.setBounds(38, 178, 189, 20);
+		panel_1.add(txtPass);
+		txtPass.setBorder(null);
+		
+		JLabel JL2 = new JLabel("Contraseña:");
+		JL2.setBounds(37, 137, 161, 30);
+		panel_1.add(JL2);
+		JL2.setFont(new Font("Roboto", Font.PLAIN, 15));
+		
+		JLabel lblNewLabel_3 = new JLabel("Iniciar Sesión");
+		lblNewLabel_3.setFont(new Font("Roboto Black", Font.PLAIN, 18));
+		lblNewLabel_3.setBounds(37, 30, 194, 14);
+		panel_1.add(lblNewLabel_3);
 		
 		JLabel lblNewLabel = new JLabel("New label");
 		lblNewLabel.setBackground(Color.LIGHT_GRAY);
@@ -102,7 +132,7 @@ public class Login extends JFrame {
 		JLabel lblNewLabel_2 = new JLabel("New label");
 		lblNewLabel_2.setHorizontalAlignment(SwingConstants.LEFT);
 		lblNewLabel_2.setIcon(new ImageIcon(Login.class.getResource("/image/pinterest_profile_image.png")));
-		lblNewLabel_2.setBounds(265, 0, 275, 208);
+		lblNewLabel_2.setBounds(282, 17, 275, 208);
 		panel.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_1 = new JLabel("New label");
@@ -111,8 +141,8 @@ public class Login extends JFrame {
 		lblNewLabel_1.setBounds(325, 66, 215, 159);
 		panel.add(lblNewLabel_1);
 		
-		JButton btnNewButton = new JButton("New button");
-		btnNewButton.setBounds(40, 264, 89, 23);
+		JButton btnNewButton = new JButton("Iniciar ");
+		btnNewButton.setBounds(40, 282, 114, 23);
 		panel.add(btnNewButton);
 	}
 }
