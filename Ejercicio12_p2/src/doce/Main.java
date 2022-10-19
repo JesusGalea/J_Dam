a016e5e7130fca30777e5080294c0841f08e84ac
JesusGalea <93735071+JesusGalea@users.noreply.github.com>
2022-10-19 21:05:23 +0200

    dd

---
+++ Ejercicio12_p2/src/doce/Main.java
@@ -0,0 +1,10 @@
+package doce;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
--- Ejercicio9_p2/src/nueve/Main.java
+++ Ejercicio9_p2/src/nueve/Main.java
@@ -3,10 +3,29 @@ package nueve;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		esCapicua(77877);
 
 	}
-
+public static boolean esCapicua (int a) {
+	int cantidad = a;
+	int resto;
+	int inverso = 0;
+	while (cantidad != 0) {
+		resto = cantidad%10;
+		cantidad = cantidad/10;
+		inverso = inverso*10+ resto;
+	}
 	
+	if (inverso == a) {
+		return true;
+	} else {
+		return false;
+	}
 	
+}
+	
+	public boolean esPrimo(int a ) {
+		
+		return false;
+	}
 }
