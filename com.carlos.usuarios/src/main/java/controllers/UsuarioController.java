d5a102ae4718970ddd52169e163b141683790990
Jesus <jrgaleasantiago98@gmail.com>
2022-11-24 13:31:30 +0100

    uwu

--- com.carlos.usuarios/src/main/java/controllers/UsuarioController.java
+++ com.carlos.usuarios/src/main/java/controllers/UsuarioController.java
@@ -62,6 +62,39 @@ public class UsuarioController {
 		return "Error al borrar el usuario";
 	}
 
+	public String updateUsuario(int id, String nombre, String apellido, String ciudad) {
+
+		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Usuario.class).buildSessionFactory();
+		
+		Session session = sessionFactory.openSession();
+		
+		try {
+			
+			
+			session.beginTransaction();
+			
+			Usuario usuario = session.get(Usuario.class, id);
+			
+			usuario.setNombre(nombre);
+			
+			usuario.setApellido(apellido);
+			
+			usuario.setCiudad(ciudad);
+			
+			session.update(usuario);
+			
+			session.getTransaction().commit();
+			
+			session.close();
+			
+			return "Usuario Actualizado";
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return "Error al actualizar el usuario";
+	}
+	
 	public String getUsuario(int id) {
 
 		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
--- com.carlos.usuarios/src/main/java/view/UsuarioView.java
+++ com.carlos.usuarios/src/main/java/view/UsuarioView.java
@@ -1,14 +1,74 @@
 package view;
 
+import java.util.Scanner;
+
 import controllers.UsuarioController;
 import models.Usuario;
 
 public class UsuarioView {
-
+	
+	
 	public static void main (String [] args) {
-		UsuarioController uc = new UsuarioController();
-		 uc.createUsuario("Alvaro", "Maricon", "Parque");
+		 boolean bucle = true;
+		 String nombre; 
+		 String apellido;
+		 String ciudad;
+		 int id;
+		 UsuarioController uc = new UsuarioController();
+		 int opcion= 0;
+		 Scanner sc = new Scanner(System.in);
+		while(bucle) {
+			
+			System.out.println("Que desea hacer?"
+					+ "\n1 - ver todos los usuarios"
+					+ "\n2 - insertar un nuevo usuario"
+					+ "\n3 - modificar un usuario"
+					+ "\n4 - buscar un usuario"
+					+ "\n5 - borrar un usuario"
+					+ "\n6 - borrar todos los usuarios"
+					+ "\n7 - salir ");
+			
+			opcion = sc.nextInt();
+			
+			switch(opcion) {
+			case 1:
+				uc.getUsuarios();
+				break;
+			case 2:
+				System.out.println("introduce el nombre");
+				 nombre = sc.next();
+				System.out.println("introduce el apellido");
+				 apellido = sc.next();
+				System.out.println("introduce la ciudad");
+				 ciudad = sc.next();
+				uc.createUsuario(nombre, apellido, ciudad);
+				break;
+			case 3:
+				System.out.println("introduce el id del usuario a modificar");
+				 id = sc.nextInt();
+				System.out.println("introduce el nuevo nombre");
+				 nombre = sc.next();
+				System.out.println("introduce el nuevo apellido");
+				 apellido = sc.next();
+				System.out.println("introduce la nueva ciudad");
+				 ciudad = sc.next();
+				uc.updateUsuario(id, nombre, apellido, ciudad);
+				break;
+			case 4:
+				System.out.println("introduce el id del usuario a buscar");
+				id = sc.nextInt();
+				uc.getUsuario(id);
+				break;
+			case 5:
+				System.out.println("introduce el id del usuario a borrar");
+				id = sc.nextInt();
+				uc.deleteUsuario(id);
+				
+			}
+			
+		}
 		
+		 		
 	}
 	
 }
